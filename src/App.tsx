f7cd4550e4518abac8c1c1826e75920b40b1bf13
Jonathan Miller <jon@jmill.dev>
2021-10-03 23:15:13 -0400

    prototype app working

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-const App = () => {
-  const [todos, setTodos] = useState([])
+enum newHighlightMethod {
+  RANDOM = 'random',
+  SIMILAR = 'similar',
+  DIFFERENT = 'different',
+}
 
-  const fetchTodos = async () => {
-    const response = await fetch('http://localhost:8000/')
-    const todos = await response.json()
-    setTodos(todos.Hello)
+const App = () => {
+  const [highlight, setHighlight] = useState('highlight')
+
+  const newHighlight = async (method: newHighlightMethod) => {
+    const data = {
+      method,
+      highlight,
+    }
+    console.log(data)
+    const response = await fetch('http://localhost:8000/highlight', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    const resp = await response.json()
+    setHighlight(resp.highlight)
   }
 
   useEffect(() => {
-    fetchTodos()
+    newHighlight(newHighlightMethod.RANDOM)
   }, [])
 
   return (
     <div>
-      <p>{todos}</p>
+      <h3>Random Highlight</h3>
+      <p>{highlight}</p>
+      <button onClick={() => newHighlight(newHighlightMethod.SIMILAR)}>Get Similar Highlight</button>
+      <br />
+      <button onClick={() => newHighlight(newHighlightMethod.DIFFERENT)}>Get Different Highlight</button>
+      <br />
+      <button onClick={() => newHighlight(newHighlightMethod.RANDOM)}>Get Random Highlight</button>
     </div>
   )
 }
