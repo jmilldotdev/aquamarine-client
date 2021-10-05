7ece7fc0a75b5739c850e8f2b9fe231a0713426a
Jonathan Miller <jon@jmill.dev>
2021-10-04 20:33:29 -0400

    update models

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,24 @@ enum newHighlightMethod {
   DIFFERENT = 'different',
 }
 
+interface Highlight {
+  corpusId: number
+  contentType: string
+  path: string
+  title: string
+  text: string
+  textEncoding: string
+}
+
 const App = () => {
-  const [highlight, setHighlight] = useState('highlight')
+  const [highlight, setHighlight] = useState<Highlight | undefined>(undefined)
 
   const newHighlight = async (method: newHighlightMethod) => {
+    const highlightText = highlight ? highlight.text : ''
     const data = {
       method,
-      highlight,
+      highlight: highlightText,
     }
-    console.log(data)
     const response = await fetch('http://localhost:8000/highlight', {
       method: 'POST',
       headers: {
@@ -24,7 +33,17 @@ const App = () => {
       body: JSON.stringify(data),
     })
     const resp = await response.json()
-    setHighlight(resp.highlight)
+    console.log(resp.highlight)
+    const responseHighlight: Highlight = {
+      corpusId: resp.highlight.corpus_id,
+      contentType: resp.highlight.content_type,
+      path: resp.highlight.path,
+      title: resp.highlight.title,
+      text: resp.highlight.text,
+      textEncoding: resp.highlight.text_encoding,
+    }
+    console.log(responseHighlight)
+    setHighlight(responseHighlight)
   }
 
   useEffect(() => {
@@ -34,7 +53,12 @@ const App = () => {
   return (
     <div>
       <h3>Random Highlight</h3>
-      <p>{highlight}</p>
+      {highlight && (
+        <div>
+          <p>{highlight.title}</p>
+          <p>{highlight.text}</p>
+        </div>
+      )}
       <button onClick={() => newHighlight(newHighlightMethod.SIMILAR)}>Get Similar Highlight</button>
       <br />
       <button onClick={() => newHighlight(newHighlightMethod.DIFFERENT)}>Get Different Highlight</button>
